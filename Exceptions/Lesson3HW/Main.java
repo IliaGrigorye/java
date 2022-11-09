579d71d22324103c24e8f23fc4d32058045f3391
Ilia Grigoryev <shirokagaricu@mail.ru>
2022-11-09 13:37:53 +0800

    added HW 3 seminar exceptions

--- Exceptions/Lesson3HW/Main.java
+++ Exceptions/Lesson3HW/Main.java
@@ -1,3 +1,4 @@
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
@@ -6,36 +7,48 @@ import java.io.FileWriter;
 public class Main {
     public static void main(String[] args){
         System.out.println("Введите свои данные через пробел ");
-        System.out.println("Пример: Иванов Петр Игоревич 15.09.1998 89834457790 муж");
+        System.out.println("Пример: Иванов Петр Игоревич 15.09.1998 89834457790 f или m");
+        System.out.print("Ввод: ");
         Scanner sc = new Scanner(System.in);
-
         String[] date = new String[6];
-        int i;
-        try(FileWriter writer = new FileWriter("File.txt")) {
-            for (i = 0; i < date.length; i++) {
+
+        try{
+            for (int i = 0; i < date.length; i++) {
                 date[i] = sc.next();
             }
+
+            String leng1 = date[3];
+            String leng2 = date[5];
+            if(leng1.length() != 10 ){
+                throw new RuntimeException("Ошибка! Не верный формат даты, введите как указано в примере");
+            }
+            if(leng2.length() != 1 ){
+                throw new RuntimeException("Ошибка! Не верно введен пол, введите как указано в примере");
+            }
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(date[0]+".txt", true));
 
             Long.parseLong(date[4]);
 
             writer.append("\n");
-            System.out.println("вы ввели: ");
-            for (i = 0; i < date.length; i++) {
+            System.out.print("Введеные данные: |");
+            for (int i = 0; i < date.length; i++) {
                 System.out.print(date[i] + " ");
                 writer.append(date[i] + " ");
             }
+            System.out.print(" |Добавлены в " + date[0] + ".txt");
+
+            writer.close();
+
 
         }catch (ArrayIndexOutOfBoundsException e){
             System.out.println("Вы ввели слишком много данных");
         }catch (NumberFormatException e){
             System.out.println("Ошибка при вводе номера телефона");
-            System.out.println(date[4]);
         }catch (FileNotFoundException e){
             System.out.println("Файл не найден");
         }catch (IOException e) {
             System.out.println("перехват ошибки: " + e.getClass().getSimpleName());
-            throw new RuntimeException(e);
         }
-
     }
 }
